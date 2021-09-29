0348a08d99275784fb7034f6130edf414279512d
Gafur Iusupaliev <igafurp@gmail.com>
2021-09-29 14:28:59 -0400

    Dashboard renders proper content depending on user's authentication status

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,4 +1,6 @@
-import { getSession } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
+import AccessDenied from "../components/accessDenied";
+import Sidebar from "../components/Sidebar/sidebar";
 import prisma from "../util/prisma";
 
 interface CurrentUser {
@@ -7,11 +9,16 @@ interface CurrentUser {
   name: string;
 }
 
+//passing user data to the dashboard
 export async function getServerSideProps(ctx) {
   const session = await getSession(ctx);
   //@ts-ignore
   const userId = session?.user?.id;
 
+  //if the user is not logged in, return an empty object
+  if (!session) return { props: {} };
+
+  //otherwise, return the user object from the database
   const currentUser = await prisma.user.findUnique({
     where: {
       id: userId,
@@ -27,9 +34,15 @@ export default function Dashboard({
 }: {
   currentUser: CurrentUser;
 }) {
-  return (
-    <>
-      <h1>hi {currentUser.name}</h1>
-    </>
-  );
+  //using the hook to get the status of authentication
+  const { status } = useSession();
+
+  //self-explanatory
+  if (status === "authenticated") {
+    return <Sidebar currentUser={currentUser} />;
+  } else if (status === "unauthenticated") {
+    return <AccessDenied />;
+  }
+
+  return <h1>loading</h1>;
 }
